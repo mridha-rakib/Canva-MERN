938c8f9416a0bfd67ddc78bf8c42b2da30646a6f
RAKIB <rakibmahmudmridha@gmail.com>
2024-01-05 12:04:00 +0600

    files modifed

--- frontend/src/components/Project.jsx
+++ frontend/src/components/Project.jsx
@@ -1,5 +1,23 @@
+import { Link } from "react-router-dom";
 const Project = () => {
-  return <div>Project</div>;
+  return (
+    <div className="h-[88vh] overflow-x-auto scrollbar-hide">
+      <div className="grid grid-cols-2 gap-2 ">
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14].map((img, i) => (
+          <Link
+            key={i}
+            className="w-full h-[90px] overflow-hidden rounded-sm cursor-pointer"
+          >
+            <img
+              className="w-full h-full object-fill"
+              src="http://localhost:5173/DIU_PIC.png"
+              alt=""
+            />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Project;
